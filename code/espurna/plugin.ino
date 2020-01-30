9c6c14935fc5582773d1f99e6151eccf734ac89b
Felix Qvist <felix.qvist@gmail.com>
2020-01-30 02:18:44 +0100

    wip

--- code/espurna/plugin.ino
+++ code/espurna/plugin.ino
@@ -3,7 +3,7 @@
 #include <Ticker.h>
 #include <WiFiClientSecureBearSSL.h>
 
-void _changeColor(const char * color)
+void _changeColor(const char *color)
 {
     lightColor(color, false);
     lightUpdate(true, true);
@@ -12,44 +12,60 @@ void _changeColor(const char * color)
 void callElectricitymap()
 {
     DEBUG_MSG_P(PSTR("[GreenLight] Calling electricityMap...\n"));
-    BearSSL::WiFiClientSecure secure;
-    secure.setInsecure();
+
+    static std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+    client->setInsecure(); // TODO: Validate SSL
     HTTPClient http;
-    http.begin(secure, "api.co2signal.com", 443, "/v1/latest?countryCode=DK-DK2", true);
-    http.addHeader("auth-token", EMAP_TOKEN);
-    int statusCode = http.GET();
-    String payload = http.getString();
-    DEBUG_MSG_P(PSTR("[GreenLight] Status Code: %i\n"), statusCode);
 
-    DynamicJsonBuffer jsonBuffer(1000);
-    JsonObject &root = jsonBuffer.parseObject(payload);
-    if (!root.success())
+    if (http.begin(*client, "https://api.co2signal.com/v1/latest?countryCode=DK-DK2"))
     {
-        DEBUG_MSG_P(PSTR("[GreenLight] Couldn't parse response: %s\n"), payload.c_str());
-        return;
+        http.addHeader("auth-token", EMAP_TOKEN);
+
+        int statusCode = http.GET();
+        if (statusCode > 0)
+        {
+            DEBUG_MSG_P(PSTR("[GreenLight] Status Code: %i\n"), statusCode);
+
+            String payload = http.getString();
+            DynamicJsonBuffer jsonBuffer(1000);
+            JsonObject &root = jsonBuffer.parseObject(payload);
+            if (!root.success())
+            {
+                DEBUG_MSG_P(PSTR("[GreenLight] Couldn't parse response: %s\n"), payload.c_str());
+                return;
+            }
+
+            float fossilFuelPercentage = root["data"]["fossilFuelPercentage"];
+            String zone = root["countryCode"];
+
+            DEBUG_MSG_P(PSTR("[GreenLight] zone: %s\n"), zone.c_str());
+            DEBUG_MSG_P(PSTR("[GreenLight] fossilFuelPercentage: %d\n"), fossilFuelPercentage);
+
+            if (fossilFuelPercentage < 10)
+                _changeColor("100,100,100");
+            else if (fossilFuelPercentage < 15)
+                _changeColor("50,100,100");
+            else if (fossilFuelPercentage < 20)
+                _changeColor("25,100,100");
+            else if (fossilFuelPercentage < 30)
+                _changeColor("15,100,100");
+            else
+                _changeColor("0,100,100");
+        }
+        else
+        {
+            DEBUG_MSG_P(PSTR("[GreenLight] GET failed, error: %s\n"), http.errorToString(statusCode).c_str());
+        }
+
+        http.end();
     }
-
-    float fossilFuelPercentage = root["data"]["fossilFuelPercentage"];
-    String zone = root["countryCode"];
-
-    DEBUG_MSG_P(PSTR("[GreenLight] zone: %s\n"), zone.c_str());
-    DEBUG_MSG_P(PSTR("[GreenLight] fossilFuelPercentage: %f\n"), fossilFuelPercentage);
-    http.end();
-
-    if (fossilFuelPercentage < 10)
-        _changeColor("100,100,100");
-    else if (fossilFuelPercentage < 15)
-        _changeColor("50,100,100");
-    else if (fossilFuelPercentage < 20)
-        _changeColor("25,100,100");
-    else if (fossilFuelPercentage < 30)
-        _changeColor("15,100,100");
     else
-        _changeColor("0,100,100");
+    {
+        DEBUG_MSG_P(PSTR("[GreenLight] Unable to connect\n"));
+    }
 }
 
 Ticker _connectionTimer;
-Ticker _syncTimer;
 Ticker _restartTimer;
 
 void noConnection()
@@ -62,6 +78,9 @@ void noConnection()
     });
 }
 
+bool _isConnectedToWifi;
+
+// Is called almost immediately after boot. Hasn't connected to wifi yet
 void extraSetup()
 {
     noConnection();
@@ -69,6 +88,7 @@ void extraSetup()
     wifiRegister([](justwifi_messages_t message, char *parameter) {
         if (MESSAGE_CONNECTED == message)
         {
+            _isConnectedToWifi = true;
             _restartTimer.detach();
             _connectionTimer.detach();
             _changeColor("300,100,100");
@@ -76,19 +96,34 @@ void extraSetup()
 
             DEBUG_MSG_P(PSTR("[GreenLight] - wifi is connected\n"));
             callElectricitymap();
-            _syncTimer.attach(360, []() {
-                callElectricitymap();
-            });
         }
         else
         {
-            _syncTimer.detach();
+            _isConnectedToWifi = false;
             DEBUG_MSG_P(PSTR("[GreenLight] - lost connection\n"));
             noConnection();
-            _restartTimer.once(300, []() {
+            _restartTimer.attach(300, []() {
                 DEBUG_MSG_P(PSTR("[GreenLight] - Rebooting...\n"));
                 deferredReset(100, CUSTOM_RESET_TERMINAL);
             });
         }
+    });
+
+    espurnaRegisterLoop([]() {
+        static auto last = millis();
+        if (millis() - last > 1000 * 300 || millis() < last) // case number two is when millis overflow (after ~50 days)
+        {
+            last = millis();
+
+            if (_isConnectedToWifi)
+            {
+                callElectricitymap();
+            }
+        }
+    });
+
+    terminalRegisterCommand(F("callemap"), [](Embedis *e) {
+        DEBUG_MSG_P(PSTR("[GreenLight] Manually called electricityMap\n"));
+        callElectricitymap();
     });
 }
